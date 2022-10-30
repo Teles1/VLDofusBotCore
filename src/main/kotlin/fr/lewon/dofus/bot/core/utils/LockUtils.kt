02e473e7c9b8fac4eb0fbc7868ab30315aeac628
viclew <viclew@gmail.com>
2022-10-30 13:03:08 +0100

    Inline executeSyncOperation

--- src/main/kotlin/fr/lewon/dofus/bot/core/utils/LockUtils.kt
+++ src/main/kotlin/fr/lewon/dofus/bot/core/utils/LockUtils.kt
@@ -4,7 +4,7 @@ import java.util.concurrent.locks.ReentrantLock
 
 object LockUtils {
 
-    fun <T> ReentrantLock.executeSyncOperation(operation: () -> T): T {
+    inline fun <T> ReentrantLock.executeSyncOperation(operation: () -> T): T {
         try {
             lockInterruptibly()
             return operation()
