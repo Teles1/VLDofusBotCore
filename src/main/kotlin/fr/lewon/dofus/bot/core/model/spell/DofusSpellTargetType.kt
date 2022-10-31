a7c6460d0469164d992d80d58edb3dcb5ecf29c1
Henrique Teles <66783472+Teles1@users.noreply.github.com>
2022-10-31 08:57:07 -0400

    Update DofusSpellTargetType.kt
    added some of the missing flags

--- src/main/kotlin/fr/lewon/dofus/bot/core/model/spell/DofusSpellTargetType.kt
+++ src/main/kotlin/fr/lewon/dofus/bot/core/model/spell/DofusSpellTargetType.kt
@@ -10,7 +10,7 @@ enum class DofusSpellTargetType(
     ALLIES('a', { _, caster, target -> caster.getFighterTeamId() == target.getFighterTeamId() }),
     ENEMIES('A', { _, caster, target -> caster.getFighterTeamId() != target.getFighterTeamId() }),
     CASTER('C', { _, caster, target -> caster.getFighterTeamId() == target.getFighterTeamId() }),
-    ALLIED_SUMMON('F', { id, _, target -> target.getPlayerType() != PlayerType.HUMAN && target.getBreed() == id }),
+    SPECIFIC_ALLIED_SUMMON('F', { id, _, target -> target.getPlayerType() != PlayerType.HUMAN && target.getBreed() == id }),
     SWITCHABLE_ENEMIES('i', { _, caster, target ->
         caster.getFighterTeamId() != target.getFighterTeamId()
                 && target.getPlayerType() != PlayerType.SIDEKICK
@@ -25,6 +25,12 @@ enum class DofusSpellTargetType(
     }),
     WITHOUT_STATE('e', { id, _, target -> id != null && !target.hasState(id) }),
     WITH_STATE('E', { id, _, target -> id != null && target.hasState(id) }),
+    ENEMY_SUMMON('J', { id, caster, target -> id != null && target.getPlayerType() != PlayerType.SIDEKICK && target.getFighterTeamId() != caster.getFighterTeamId() && target.isSummon()}),
+    ALLIED_SUMMON('j', {id, caster, target -> id != null && target.getFighterTeamId() == caster.getFighterTeamId() && target.getPlayerType() != PlayerType.SIDEKICK && target.isSummon()}),
+    ENEMY_PLAYER('L',{id, caster, target -> id != null && target.getFighterTeamId() != caster.getFighterTeamId() && (target.getPlayerType() == PlayerType.HUMAN && !target.isSummon() || target.getPlayerType() == PlayerType.SIDEKICK)}),
+    ALLIED_PLAYER('l',{id, caster, target -> id != null && target.getFighterTeamId() == caster.getFighterTeamId() && (target.getPlayerType() == PlayerType.HUMAN && !target.isSummon() || target.getPlayerType() == PlayerType.SIDEKICK)}),
+    NON_HUMAN_NON_SUMMON_ALLY('m',{id, caster, target -> id != null && target.getFighterTeamId() == caster.getFighterTeamId() && target.getPlayerType() != PlayerType.HUMAN && !target.isSummon() && !target.isStaticElement()}),
+    NON_HUMAN_NON_SUMMON_ENEMY('M',{id, caster, target -> id != null && target.getFighterTeamId() != caster.getFighterTeamId() && target.getPlayerType() != PlayerType.HUMAN && !target.isSummon() && !target.isStaticElement()})
     ;
 
     companion object {
